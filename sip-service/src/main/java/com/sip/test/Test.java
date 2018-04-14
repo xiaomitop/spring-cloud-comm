dd332d3c557c90a85931a256336e7a48b2e1885f
yangt <yangtaotop@126.com>
2018-04-14 18:10:42 +0800

    加入网关zuul

---
+++ sip-service/src/main/java/com/sip/test/Test.java
@@ -0,0 +1,16 @@
+package com.sip.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class Test {
+    @RequestMapping("test")
+    @ResponseBody
+    public String test() {
+        System.out.println("-------------------------------");
+        return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    }
+}
