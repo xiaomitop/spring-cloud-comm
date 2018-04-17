8f10305e644023b964ce621b98ce798633523861
yangt <yangtaotop@126.com>
2018-04-17 18:16:04 +0800

    加入配置中心

--- sip-service/src/main/java/com/sip/SipServerApplication.java
+++ sip-service/src/main/java/com/sip/SipServerApplication.java
@@ -1,14 +1,23 @@
 package com.sip;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SipServerApplication {
+    /*@Value("${url}")
+    String url;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SipServerApplication.class, args);
-	}
+    @RequestMapping("/get")
+    String hello() {
+        return "Hello " + url + "!";
+    }*/
+
+    public static void main(String[] args) {
+        SpringApplication.run(SipServerApplication.class, args);
+    }
 }
